98375e112a4ee5172ae9c31cecad2af230e15d26
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-03 10:30:41 +0530

    first c program

---
+++ first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // declaring varialble to store data 
+    int name;
+    int surname;
+    // declaring variable to store error codes 
+    int error_code;
+    printf("Hello World, I am learing C");
+    return 0;
+}
