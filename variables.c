12087fede9d5e0aeee6e93f88ffdf7de0dcbde70
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-06 10:36:06 +0530

    variables in C

---
+++ variables.c
@@ -0,0 +1,11 @@
+// Variables in C programming 
+
+#include<stdio.h>
+
+int main() {
+    int a = 4;  //integer
+    float b = 3.5;  // float
+    char c = 'k'; //character
+    printf("The value of a is %d, and b is %f", a, b);
+    return 0;
+}
