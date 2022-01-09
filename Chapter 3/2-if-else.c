a0c74239890864dbeb4c3263089353aecdfab1eb
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-09 16:17:40 +0530

    if else conditional

---
+++ Chapter 3/2-if-else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: \n");
+    scanf("%d", &age);
+
+    if (age < 18)
+    {
+        printf("You cannot drive! \n");
+    }
+    else
+    {
+        printf("You can drive! \n");
+    }
+
+    if (age == 50)
+    {
+        printf("Half century! \n");
+    }
+    
+    return 0;
+}
